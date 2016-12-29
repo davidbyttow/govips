e1c2e598f3f2964a3dd207cacf097f88c04a598c
David Byttow <guitardave@gmail.com>
2016-12-29 00:32:52 -0800

    Added avg

---
+++ examples/avg.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/davidbyttow/gimage"
+	"github.com/davidbyttow/gomore/io"
+)
+
+const usage = "avg [input]"
+
+func loadAverage(file string) error {
+	buf, err := io.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	image, err := gimage.LoadBuffer(buf)
+	if err != nil {
+		return err
+	}
+
+	avg := image.Avg(nil)
+	fmt.Printf("avg=%0.2f\n", avg)
+
+	return nil
+}
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, usage)
+	}
+	flag.Parse()
+
+	file := os.Args[1]
+
+	defer gimage.Shutdown()
+	err := loadAverage(file)
+	if err != nil {
+		os.Exit(1)
+	}
+}
