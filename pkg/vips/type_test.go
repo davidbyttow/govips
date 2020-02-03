ee0679ea82287b729b3b0ab63d7a60ff86fc6b10
John Ford <john@johnford.org>
2020-02-03 16:06:53 +0100

    restructure type tests and add other types

--- pkg/vips/type_test.go
+++ pkg/vips/type_test.go
@@ -8,10 +8,27 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestJPEG(t *testing.T) {
-	buf, _ := ioutil.ReadFile("../../assets/fixtures/canyon.jpg")
-	assert.NotNil(t, buf)
+func TestTypes(t *testing.T) {
+  tests := map[string]vips.ImageType{
+    "koala.jpg": vips.ImageTypeJPEG,
+    //"koala.webp": vips.ImageTypeWEBP,
+    "koala.png": vips.ImageTypePNG,
+    //"koala.tiff": vips.ImageTypeTIFF,
+    //"koala.gif": vips.ImageTypeGIF,
+    //"koala.bmp": vips.ImageTypeBMP,
+    //"koala.bmp2": vips.ImageTypeBMP,
+    //"koala.bmp3": vips.ImageTypeBMP,
+  }
 
-	imageType := vips.DetermineImageType(buf)
-	assert.Equal(t, vips.ImageTypeJPEG, imageType)
+  for input, expected := range tests {
+    input := input
+    expected := expected
+
+    buf, _ := ioutil.ReadFile("../../assets/fixtures/" + input)
+    assert.NotNil(t, buf)
+
+    imageType := vips.DetermineImageType(buf)
+    assert.Equal(t, expected, imageType,
+      "%s <> %s", expected.OutputExt(), imageType.OutputExt())
+  }
 }
