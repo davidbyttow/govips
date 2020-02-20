e5bd9c32399c9ca1dfa1e83d726f2c231e26e736
elad laufer <elad@wix.com>
2020-02-20 10:59:35 +0200

    - always subsample JPEG regardless of Q

--- vips/foreign.c
+++ vips/foreign.c
@@ -79,6 +79,7 @@ int save_jpeg_buffer(VipsImage *in, void **buf, size_t *len, int strip, int qual
 		"Q", quality,
 		"optimize_coding", TRUE,
 		"interlace", INT_TO_GBOOLEAN(interlace),
+		"subsample_mode", VIPS_FOREIGN_JPEG_SUBSAMPLE_ON,
 		NULL
 	);
 }
