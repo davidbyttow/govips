315a787ab7c9c32bed620e0ffff022249ac79828
Paul van Santen <pvansanten@transip.nl>
2020-12-10 10:59:20 +0100

    Add benchmark for the exporting of an image

--- vips/image_test.go
+++ vips/image_test.go
@@ -472,6 +472,24 @@ func TestImageRef_CompositeMulti(t *testing.T) {
 	require.NoError(t, err)
 }
 
+func BenchmarkCreateNewImage(b *testing.B) {
+	Startup(nil)
+
+	fileBuf, err := ioutil.ReadFile(resources + "heic-24bit.heic")
+	require.NoError(b, err)
+
+	b.SetParallelism(100)
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		img, err := NewImageFromBuffer(fileBuf)
+		require.NoError(b, err)
+
+		_, _, err = img.Export(NewDefaultJPEGExportParams())
+		require.NoError(b, err)
+	}
+	b.ReportAllocs()
+}
+
 // TODO Add unit tests for:
 // Copy
 // Close
