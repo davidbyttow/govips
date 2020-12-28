3bff0cf3c1d174aa8ded9e0ae6d71f28d4fa6fdb
Toni Melisma <toni.melisma@iki.fi>
2020-12-28 19:34:18 +0200

    Fix issue #63, if vips compiled without ICC

--- vips/color.c
+++ vips/color.c
@@ -1,35 +1,50 @@
 #include "color.h"
 #include <unistd.h>
 
-int is_colorspace_supported(VipsImage *in) {
+int is_colorspace_supported(VipsImage *in)
+{
 	return vips_colourspace_issupported(in) ? 1 : 0;
 }
 
-int to_colorspace(VipsImage *in, VipsImage **out, VipsInterpretation space) {
+int to_colorspace(VipsImage *in, VipsImage **out, VipsInterpretation space)
+{
 	return vips_colourspace(in, out, space, NULL);
 }
 
 // https://libvips.github.io/libvips/API/8.6/libvips-colour.html#vips-icc-transform
-int optimize_icc_profile(VipsImage *in, VipsImage **out, int isCmyk, char *srgb_profile_path, char *gray_profile_path) {
+int optimize_icc_profile(VipsImage *in, VipsImage **out, int isCmyk, char *srgb_profile_path, char *gray_profile_path)
+{
 	// todo: check current embedded profile, and skip if already set
 
-    int channels = vips_image_get_bands(in);
+	int channels = vips_image_get_bands(in);
 	int result;
 
-    if (channels > 2) {
-    	if (isCmyk == 1) {
-    		result = vips_icc_transform(in, out, srgb_profile_path, "input_profile", "cmyk", "intent", VIPS_INTENT_PERCEPTUAL, NULL);
-    	} else {
-        	result = vips_icc_transform(in, out, srgb_profile_path, "embedded", TRUE, "intent", VIPS_INTENT_PERCEPTUAL, NULL);
-        	// ignore embedded errors
-        	if (result != 0) {
-        		result = 0;
-        		*out = in;
-        	}
-    	}
-    } else {
-			result = vips_icc_transform(in, out, gray_profile_path, "input_profile", gray_profile_path, "embedded", TRUE, "intent", VIPS_INTENT_PERCEPTUAL, NULL);
-    }
+	if (vips_icc_present() == 0)
+	{
+		return 1;
+	}
 
-    return result;
+	if (channels > 2)
+	{
+		if (isCmyk == 1)
+		{
+			result = vips_icc_transform(in, out, srgb_profile_path, "input_profile", "cmyk", "intent", VIPS_INTENT_PERCEPTUAL, NULL);
+		}
+		else
+		{
+			result = vips_icc_transform(in, out, srgb_profile_path, "embedded", TRUE, "intent", VIPS_INTENT_PERCEPTUAL, NULL);
+			// ignore embedded errors
+			if (result != 0)
+			{
+				result = 0;
+				*out = in;
+			}
+		}
+	}
+	else
+	{
+		result = vips_icc_transform(in, out, gray_profile_path, "input_profile", gray_profile_path, "embedded", TRUE, "intent", VIPS_INTENT_PERCEPTUAL, NULL);
+	}
+
+	return result;
 }
